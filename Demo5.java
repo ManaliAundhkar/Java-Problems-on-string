8e483c0454437606b2c4e13d3b21ebaa4da5f39a
ManaliAundhkar <56449889+ManaliAundhkar@users.noreply.github.com>
2020-02-27 17:27:09 +0530

    Java programs on string

---
+++ Demo5.java
@@ -0,0 +1,65 @@
+//Demo5.java
+import java.util.*;
+import java.lang.*;
+
+
+class Line4
+{
+	public String ReverseX(String strr)
+	{
+		
+		char temp;
+		String reverse = "";
+        
+        
+        for(int i = strr.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + strr.charAt(i);
+        }
+        
+        System.out.println("Reversed string is:");
+        System.out.println(reverse);
+		
+		return (reverse);
+		
+		
+		/*char Array[]=strr.toCharArray();  
+		int iStart=0;
+		int iEnd=(Array.length)-1;
+	
+		
+		for(iStart=0;iStart<iEnd;iStart++,iEnd--)
+		{
+			
+				temp=Array[iStart];
+				Array[iStart]=Array[iEnd];
+				Array[iEnd]=temp;
+			
+			
+		}
+		System.out.println(Array.toString());
+		return (Array.toString());*/
+		
+	}
+}
+
+class Demo5
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj=new Scanner(System.in);
+		
+		System.out.println("Enter the String");
+		String str=sobj.nextLine();
+		
+		Line4 lobj=new Line4();
+		
+		String rev = "";
+		
+		rev=lobj.ReverseX(str);
+		
+		
+			System.out.println("String is"+rev);
+		
+	}
+}
